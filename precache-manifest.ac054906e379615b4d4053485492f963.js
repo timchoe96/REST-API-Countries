d948f94dc4b9a5ae7f9d46e62df5349bed12bb39
Timothy Choe <timothychoe@MacBook-Pro.local>
2020-07-08 21:21:38 -0400

    Updates

---
+++ precache-manifest.ac054906e379615b4d4053485492f963.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d796928a673cdc8ba53bfae591732ed2",
+    "url": "/REST-API-Countries/index.html"
+  },
+  {
+    "revision": "cf58a08574ea63126cd1",
+    "url": "/REST-API-Countries/static/js/2.45d70635.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/REST-API-Countries/static/js/2.45d70635.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "20dade022efb624617da",
+    "url": "/REST-API-Countries/static/js/main.fe160c25.chunk.js"
+  },
+  {
+    "revision": "61ea8672c68d73b939e5",
+    "url": "/REST-API-Countries/static/js/runtime-main.a7879843.js"
+  }
+]);
